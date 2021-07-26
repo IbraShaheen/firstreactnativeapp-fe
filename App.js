354ac1aaac3511de9283bbae76ae6625e5c29fa4
IbraShaheen <engibraheem1995@gmail.com>
2021-07-27 02:18:21 +0300

    fixed

--- App.js
+++ App.js
@@ -1,35 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from "./components/home"
-import { Provider } from 'react-redux';
-import store from './store/reducers/';
-import ShopList from './components/shopList';
-import { NativeBaseProvider } from 'native-base';
-import ShopDetail from './components/ShopDetail';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./components/home";
+import { Provider } from "react-redux";
+import store from "./store/reducers/";
+import ShopList from "./components/shopList";
+import { NativeBaseProvider } from "native-base";
+import ShopDetail from "./components/ShopDetail";
 
 import { NavigationContainer } from "@react-navigation/native";
-import StackNavigator from './components/Navigation';
-
+import StackNavigator from "./components/Navigation";
 
 export default function App() {
   return (
     <Provider store={store}>
       <NativeBaseProvider>
+        {/* <View style={styles.container}> */}
+        <NavigationContainer>
+          <StackNavigator />
 
-    {/* <View style={styles.container}> */}
-      <NavigationContainer>
-
-        <StackNavigator/>
-
-     {/* <Home/>
+          {/* <Home/>
      <ShopList/>
      <ShopDetail/> */}
-     </NavigationContainer>
-    {/* </View> */}
-      <StatusBar style="auto" />
-    </NativeBaseProvider>
-
+        </NavigationContainer>
+        {/* </View> */}
+        <StatusBar style="auto" />
+      </NativeBaseProvider>
     </Provider>
   );
 }
@@ -37,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- store/action/authActions.js
+++ store/action/authActions.js
@@ -1,23 +1,22 @@
 import { SET_USER } from "./types";
 import instance from "./instance";
 import decode from "jwt-decode";
-import { AsyncStorage } from "react-native";
-import { useDispatch } from "react-redux";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { CART_LIST } from "../../components/Navigation/types";
+
+
 
 export const signup = (userData, navigation) => {
   return async (dispatch) => {
     try {
       const res = await instance.post("/signup", userData);
       dispatch(setUser(res.data.token));
-      navigation.navigate(CART_LIST)
-
+      navigation.navigate(CART_LIST);
     } catch (error) {
       console.log(error.message);
     }
   };
 };
-
 
 
 export const signin = (userData, navigation) => {
@@ -26,7 +25,7 @@ export const signin = (userData, navigation) => {
       const res = await instance.post("/signin", userData);
       dispatch(setUser(res.data.token));
       // console.log(res.data.token);
-      navigation.goBack()
+      navigation.goBack();
     } catch (error) {
       console.log(error.message);
     }
@@ -34,72 +33,43 @@ export const signin = (userData, navigation) => {
 };
 
 
-
-//history as prop from Nav Bar
 export const signout = () => {
- 
   return setUser();
 };
 
 
-
-export const checkForToken = async () => {
+export const checkForToken = async (dispatch) => {
   //    token = localStorage.getItem("myToken")
-         const token = await AsyncStorage.getItem("myToken")
-              const dispatch = useDispatch()
+
+  const token = await AsyncStorage.getItem("myToken");
 
   if (token) {
     //check if token expiered or not when the user refresh the page
     const currentTime = Date.now();
     const user = decode(token);
-    if (user.exp > currentTime) return dispatch(setUser(token))
-    
+    if (user.exp > currentTime) return dispatch(setUser(token));
   }
-  dispatch(setUser())
+  dispatch(setUser());
 };
 
-
-
 const setUser = (token) => async (dispatch) => {
-    if (token) {
-      instance.defaults.headers.common.Authorization = `Bearer ${token}`;
-      await AsyncStorage.setItem("myToken", token);
-      return dispatch({
-        type: SET_USER,
-        //decode enable me to see what is inside the toke it self(the pay load that i make it inside the BE, so it is an object)
-        payload: decode(token),
-      });
-    } else {
-      delete instance.defaults.headers.common.Authorization;
-  
-      await AsyncStorage.removeItem("myToken");
-      return dispatch({
-        type: SET_USER,
-        payload: null,
-      });
-    }
-  };
+  if (token) {
+    instance.defaults.headers.common.Authorization = `Bearer ${token}`;
 
+    await AsyncStorage.setItem("myToken", token);
 
+    return dispatch({
+      type: SET_USER,
+      //decode enable me to see what is inside the toke it self(the payload that i make it inside the BE, so it is an object)
+      payload: decode(token),
+    });
+  } else {
+    delete instance.defaults.headers.common.Authorization;
 
-
-// const setUser = (token) => {
-//   if (token) {
-//     instance.defaults.headers.common.Authorization = `Bearer ${token}`;
-
-//     // localStorage.setItem("myToken", token)
-//     return {
-//       type: SET_USER,
-//       payload: decode(token),
-//     };
-//   } else {
-//     delete instance.defaults.headers.common.Authorization;
-//     // to delete the token when we press logout button
-
-//     localStorage.removeItem("myToken");
-//     return {
-//       type: SET_USER,
-//       payload: null,
-//     };
-//   }
-// };
+    await AsyncStorage.removeItem("myToken");
+    return dispatch({
+      type: SET_USER,
+      payload: null,
+    });
+  }
+};
